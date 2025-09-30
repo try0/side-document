be3200cd2c835d0fa946def64f7a004c476b19fd
Ryo <try0.development@gmail.com>
2025-09-30 18:36:56 +0900

    デモ

--- side-document-js/src/DemoApp.svelte
+++ side-document-js/src/DemoApp.svelte
@@ -194,17 +194,17 @@
 </script>
 
 <main class="app-container">
-    <div>
-        <h1>Side Document Example</h1>
+    <div style="display:flex; align-items: center; gap: 0.5rem; padding-bottom: 0.5rem;">
+        <h1 style="flex:1;">Side Document Example</h1>
         <a href="https://github.com/try0/side-document"
             ><img
                 src="data:image/svg+xml,%3csvg%20width='98'%20height='96'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20fill-rule='evenodd'%20clip-rule='evenodd'%20d='M48.854%200C21.839%200%200%2022%200%2049.217c0%2021.756%2013.993%2040.172%2033.405%2046.69%202.427.49%203.316-1.059%203.316-2.362%200-1.141-.08-5.052-.08-9.127-13.59%202.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015%204.934.326%207.523%205.052%207.523%205.052%204.367%207.496%2011.404%205.378%2014.235%204.074.404-3.178%201.699-5.378%203.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283%200-5.378%201.94-9.778%205.014-13.2-.485-1.222-2.184-6.275.486-13.038%200%200%204.125-1.304%2013.426%205.052a46.97%2046.97%200%200%201%2012.214-1.63c4.125%200%208.33.571%2012.213%201.63%209.302-6.356%2013.427-5.052%2013.427-5.052%202.67%206.763.97%2011.816.485%2013.038%203.155%203.422%205.015%207.822%205.015%2013.2%200%2018.905-11.404%2023.06-22.324%2024.283%201.78%201.548%203.316%204.481%203.316%209.126%200%206.6-.08%2011.897-.08%2013.526%200%201.304.89%202.853%203.316%202.364%2019.412-6.52%2033.405-24.935%2033.405-46.691C97.707%2022%2075.788%200%2048.854%200z'%20fill='%2324292f'/%3e%3c/svg%3e"
                 style="height: 2rem;width: 2rem;"
             /></a
         >
-        <p>使い方とサンプル</p>
     </div>
-    <div style="padding: 0.5rem 0;">
+    <div style="padding: 0.5rem 0; display: flex; gap: 0.5rem;">
+        <div style="flex: auto"></div>
         <button on:click={resetOption} class="button-base">リセット</button>
     </div>
 
@@ -922,7 +922,6 @@
     <!-- SideDocumentコンポーネント -->
     <!-- <SideDocumentContainer {option} /> -->
 </main>
-
 <style>
     .app-container {
         font-family: Arial, sans-serif;
@@ -1285,12 +1284,16 @@
         justify-content: center;
         text-align: center;
         color: white;
-        background-color: var(--sd-primary-color, #236ad4);
+        background-color: #236ad4;
         border: none;
         box-shadow: 2px 0 8px rgba(0, 0, 0, 0.15);
         padding: 0.25rem;
         border-radius: 0.25em;
         cursor: pointer;
         z-index: var(--sd-drawer-z-index, 1000);
+    }
+
+    .button-base:hover {
+        background-color: #1a4f9c;
     }
 </style>
